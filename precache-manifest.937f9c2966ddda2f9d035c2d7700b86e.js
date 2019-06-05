ed1c34837fe1cb306d59676155a8936abd189978
edwardsun007 <edwardsun007@gmail.com>
2019-06-04 19:32:42 -0700

    Updates

---
+++ precache-manifest.937f9c2966ddda2f9d035c2d7700b86e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "359d8fa45200259bb2c3e0a837493142",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "32a2ff0abc360b833498",
+    "url": "/seasons/static/css/main.908dfd7e.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "32a2ff0abc360b833498",
+    "url": "/seasons/static/js/main.be3beb32.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
